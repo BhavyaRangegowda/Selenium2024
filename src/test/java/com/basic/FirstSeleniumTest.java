eb01969caf890971c87fca29d78665a6bac4f5b8
Bhavya Rangegowda <bhavyamrangegowda@gmail.com>
2024-07-04 11:21:03 +1200

    first file to launch the browser

---
+++ src/test/java/com/basic/FirstSeleniumTest.java
@@ -0,0 +1,73 @@
+package com.basic;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Navigation;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.*;
+public class FirstSeleniumTest {
+    public static void main(String[] args) throws InterruptedException {
+        // TODO Auto-generated method stub
+        System.out.println("Open Maven Website");
+
+        //create instance of chromeDriver class
+        //interface referencevariable=new implemnetdclass();
+        WebDriver driver = new ChromeDriver();
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+        //launch the url
+        driver.get("https://www.facebook.com/");
+
+        //get the title of the current page - getTitle()
+        String title=driver.getTitle();
+        System.out.println("Current page title is: " +title);
+
+        //get current page absolute url
+        String url= driver.getCurrentUrl();
+        System.out.println("Current page url is: " +url);
+
+        //Current page source code
+        String src = driver.getPageSource();
+       //System.out.println("current page source is: "+src);
+
+        //fetch current window  id ( Interview question)
+        String wid=driver.getWindowHandle();
+        System.out.println("current page window iud is: "+wid);
+
+        //fetch all the opened window id's
+        Set<String>handles = driver.getWindowHandles();
+        System.out.println("All the window id's are: "+handles);
+
+        if(title.contains("Facebook"))
+        {
+            System.out.println("Test case is Pass. Title found the word: " +title);
+        }
+        else
+        {
+            System.out.println("Test case failed. Word Maven not found. Title is:");
+        }
+        //find element email field
+        WebElement emailInput = driver.findElement(By.id("email"));
+        emailInput.clear();
+        //Input username
+        emailInput.sendKeys("bhavya@yahoo.com");
+
+        //find element password field
+        WebElement passInput = driver.findElement(By.id("pass"));
+        passInput.clear();
+        //Input password
+        passInput.sendKeys("aaaa");
+
+        WebElement loginbutton = driver.findElement(By.id("loginbutton"));
+        loginbutton.click();
+        Thread.sleep(4000);
+        //close the browser
+        //driver.close();
+
+        //quit entire webdriver. All the webdriver instances will be closed (Interview question)
+        //driver.quit();
+    }
+}
